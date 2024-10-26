866a611a62d5af4475bc616bf0027e53d2c56f93
Ricardo Farias <ricardofariasxyz@gmail.com>
2024-10-26 19:43:42 -0300

    Structuring the JSON utility and Exception configuration

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/config/exception/ExceptionGlobalHandler.java
@@ -0,0 +1,14 @@
+package br.com.microservices.orchestrated.orderservice.config.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionGlobalHandler {
+
+    public ResponseEntity<?> handleValidationException(ValidationException validationException) {
+        var details = new ExceptionDetails(HttpStatus.BAD_REQUEST.value(), validationException.getMessage());
+        return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
+    }
+}
